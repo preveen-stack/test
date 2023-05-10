eb618a7e6c8335ba5239a401a1775fa99273c4e5
Preveen P <31464911+preveen-stack@users.noreply.github.com>
2023-05-10 13:38:36 +0530

    Create loop.c

---
+++ learnc/loop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define LOOP while(1)
+#define SUCCESS (0)
+
+int main()
+{
+        LOOP {
+                printf("hello world");
+        }
+
+        return SUCCESS;
+}
+~
